599164dd7403c1a21658f7a7e675ea399a795ccb
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2023-06-13 10:07:23 +0530

    first commit

---
+++ src/content/data.js
@@ -0,0 +1,65 @@
+ export const data =[
+    {
+      "name": "Photo 1",
+      "url": "/images/r1.jpg",
+      "description": "Photo 1 description",
+      "date": "2020-01-01",
+      "type": "image/jpg"
+    },
+    {
+      "name": "Photo 2",
+      "url": "/images/r2.jpg",
+      "description": "Photo 2 description",
+      "date": "2020-02-02",
+      "type": "image/jpg"
+    },
+    {
+      "name": "Photo 3",
+      "url": "/images/r3.jpg",
+      "description": "Photo 3 description",
+      "date": "2020-03-03",
+      "type": "image/jpg"
+    },
+    {
+      "name": "Photo 4",
+      "url": "/images/r4.jpg",
+      "description": "Photo 4 description",
+      "date": "2020-04-04",
+      "type": "image/jpg"
+    },
+    {
+      "name": "Photo 5",
+      "url": "/images/r5.jpg",
+      "description": "Photo 5 description",
+      "date": "2020-05-05",
+      "type": "image/jpg"
+    },
+    {
+      "name": "Photo 6",
+      "url": "/images/r6.jpg",
+      "description": "Photo 6 description",
+      "date": "2020-06-06",
+      "type": "image/jpg"
+    },
+    {
+      "name": "Photo 7",
+      "url": "/images/r7.jpg",
+      "description": "Photo 7 description",
+      "date": "2020-07-07",
+      "type": "image/jpg"
+    },
+    {
+      "name": "Photo 8",
+      "url": "/images/r8.jpg",
+      "description": "Photo 8 description",
+      "date": "2020-08-08",
+      "type": "image/jpg"
+    },
+    {
+      "name": "Photo 9",
+      "url": "/images/r9.jpg",
+      "description": "Photo 9 description",
+      "date": "2020-09-09",
+      "type": "image/jpg"
+    }
+  ]
